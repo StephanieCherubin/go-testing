f705568f57e1b2718d7af362ce0cc6cf800cfb8d
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-11-25 17:04:18 -0800

    [add] main functions

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Calculate returns x + 2
+func Calculate(x int) (result int) {
+	result = x + 2
+	return result
+}
+
+func main() {
+	fmt.Println("Testing...")
+	result := Calculate(2)
+	fmt.Println(result)
+}
